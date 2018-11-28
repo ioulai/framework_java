ba7772b8f38239b94230d9bfa590ea499019961b
gantispam <gael.antispam@gmail.com>
2018-11-28 14:51:58 +0100

    TD : relation 1-1 bidirection

---
+++ 03_TD_Proposition/M03_05_Relation_1-1-bidirection/src/main/java/fr/demo/jpa/entity/PersonneEntity.java
@@ -0,0 +1,96 @@
+package fr.demo.jpa.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity(name = "Personne")
+@Table(name = "personne")
+public class PersonneEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nom;
+    private String prenom;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private AdresseEntity adresse;
+
+    public PersonneEntity() {
+    }
+
+    public PersonneEntity(String nom, String prenom) {
+        super();
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public PersonneEntity(String nom, String prenom, AdresseEntity adresse) {
+        super();
+        this.nom = nom;
+        this.prenom = prenom;
+        setAdresse(adresse);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public AdresseEntity getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(AdresseEntity adresse) {
+        this.adresse = adresse;
+        // IMPORTANT BIND de la personne avec l'adresse !
+        if (adresse.getPersonne() == null) {
+            adresse.setPersonne(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("PersonneEntity [id=");
+        builder.append(id);
+        builder.append(", nom=");
+        builder.append(nom);
+        builder.append(", prenom=");
+        builder.append(prenom);
+        builder.append(", adresse=");
+        builder.append(adresse);
+        builder.append("]");
+        return builder.toString();
+    }
+
+}
